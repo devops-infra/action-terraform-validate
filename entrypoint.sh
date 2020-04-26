9c32a4a077781f4ffa5ce1df8fd2b29d2993f879
Krzysztof Szyper <krzysztof.szyper@gmail.com>
2020-04-26 19:57:14 +0200

    Updated configuration after repo transfer to devops-infra

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,13 +11,14 @@ IFS=',' read -r -a ARRAY <<< "${INPUT_DIR_FILTER}"
 # Go through all dir prefixes
 for PREFIX in "${ARRAY[@]}"; do
     # Go through all matching directories
-    for DIRECTORY in $(ls -d ${PREFIX}*/); do
-        cd ${DIRECTORY}
+    for DIRECTORY in "${PREFIX}"*; do
+        [[ -d "${DIRECTORY}" ]] || break
+        cd "${WORK_DIR}/${DIRECTORY}" || RET_CODE=1
         echo -e "\nDirectory: ${DIRECTORY}"
         terraform init || RET_CODE=1
         terraform validate || RET_CODE=1
         rm -rf .terraform/
-        cd ${WORK_DIR}
+        cd "${WORK_DIR}" || RET_CODE=1
     done
 done
 
